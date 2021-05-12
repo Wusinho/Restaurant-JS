3c3c2898e6594ca735ff9f9eda3cab106aea59e6
Heber <heberlazo@gmail.com>
2021-05-11 21:56:57 -0500

    Added pictures

--- src/controllers/error404.controller.js
+++ src/controllers/error404.controller.js
@@ -2,7 +2,6 @@ import views from "../views/error404.html";
 
 export default () => {
   const divElement = document.createElement("div");
-  divElement.classList = "text-white";
   divElement.innerHTML = views;
   return divElement;
 };
--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -2,7 +2,6 @@ import views from "../views/home.html";
 
 export default () => {
   const divElement = document.createElement("div");
-  divElement.classList = "text-white";
   divElement.innerHTML = views;
   return divElement;
 };
