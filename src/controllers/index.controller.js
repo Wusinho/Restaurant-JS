cf56f6a18b2180f0c94d038859479b48c7c8e7c7
Heber <heberlazo@gmail.com>
2021-05-11 21:28:37 -0500

    Added controllers

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,8 @@
+import views from "./views/home.html";
+
+export default () => {
+  const divElement = document.createElement("div");
+  divElement.classList = "text-white";
+  divElement.innerHTML = views;
+  return divElement;
+};
